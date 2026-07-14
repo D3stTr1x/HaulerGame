896c4515da5a410adf3b626d80c8df1a7a501537
NeGr0DeV <staryidedos@gmail.com>
2026-07-14 15:26:07 +0300

    da

---
+++ Assets/Scripts/Cargo/CargoBottle.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CargoBottle : CargoBase
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = 15f;
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
