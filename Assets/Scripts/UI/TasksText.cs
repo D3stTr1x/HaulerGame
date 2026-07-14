857ad0c92c16c990f61f780b89f52ad8c4dd6aa0
NeGr0DeV <staryidedos@gmail.com>
2026-07-14 17:36:05 +0300

    a

--- Assets/Scripts/UI/TasksText.cs
+++ Assets/Scripts/UI/TasksText.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 public class TasksText : MonoBehaviour
 {
     private TextMeshProUGUI tasksText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tasksText = gameObject.GetComponent<TextMeshProUGUI>();
-        UpdateTasksTextDisplay();
+        tasksText.text = "Current tasks:\n";
+        //AddText();
     }
-    void UpdateTasksTextDisplay()
+    void AddText(string text)
     {
-        tasksText.text = "Current tasks:\n õç";
+        //tasksText.text = "Current tasks:\n";
+        tasksText.text += text;
+    }
+    void FormDeliveryTask()
+    {
+        int rnd = (int)Random.Range(1, 11);
+        string mes = $"Deliver Any cargo ({0}/{rnd})";
     }
     // Update is called once per frame
     void Update()
